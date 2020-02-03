41c96e38b43337b5cb6848b261e1a71b9c55c888
Mark Drummond <dmark@users.noreply.github.com>
2020-02-02 20:23:31 -0500

    Create add-uuid-to-idtoken.js

---
+++ rules/add-uuid-to-idtoken.js
@@ -0,0 +1,10 @@
+function (user, context, callback) {
+  var namespace = "https://leanangle.io/claims/";
+  
+  user.app_metadata = user.app_metadata || {};
+  user.app_metadata.uuid = user.app_metadata.uuid || {};
+  
+  context.idToken[namespace + "uuid"] = user.user_metadata.uuid;
+  
+  callback(null, user, context);
+}
