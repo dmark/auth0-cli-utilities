8b25596f89556fa6fc9245c3031930e79c51fb51
Mark Drummond <dmark@users.noreply.github.com>
2020-02-21 22:28:24 -0500

    Fixes reference to app_metadata.

--- rules/add-uuid-to-idtoken.js
+++ rules/add-uuid-to-idtoken.js
@@ -4,7 +4,7 @@ function (user, context, callback) {
   user.app_metadata = user.app_metadata || {};
   user.app_metadata.uuid = user.app_metadata.uuid || {};
   
-  context.idToken[namespace + "uuid"] = user.user_metadata.uuid;
+  context.idToken[namespace + "uuid"] = user.app_metadata.uuid;
   
   callback(null, user, context);
 }
